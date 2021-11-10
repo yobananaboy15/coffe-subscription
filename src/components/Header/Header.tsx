5d5ffeb8dc6fe8ce3df2de5af3655ade680799af
Axel Thiel <axth2220@gmail.com>
2021-11-10 16:30:57 +0100

    add viewport hook to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import HeaderLogo from '../HeaderLogo/HeaderLogo';
 import HeaderItem, { items } from '../HeaderItem/HeaderItem';
+import useViewport from '../../hooks/useViewport';
+import { breakpoints } from '../../constants';
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <HeaderLogo />
-    <div className={styles.headerItemsContainer}>
-      {items.map((item) => <HeaderItem itemDetails={item} />)}
-    </div>
-  </header>
-);
+const Header = () => {
+  const { width } = useViewport();
+
+  return (
+    <header className={styles.header}>
+      <HeaderLogo />
+      <div className={styles.headerItemsContainer}>
+        {items.map((item) => <HeaderItem itemDetails={item} />)}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
