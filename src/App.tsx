66471dcffe8a2d67b27f84db76f9818cbcf0af5d
Axel Thiel <axth2220@gmail.com>
2021-11-11 12:56:43 +0100

    adding assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import AboutUs from './pages/AboutUs';
 import Home from './pages/Home';
 import Subscribe from './pages/Subscribe';
 import Header from './components/Header/Header';
+import AppContainer from './components/AppContainer/AppContainer';
 import './styles/index.scss';
 
 function App() {
   return (
-    <div>
+    <AppContainer>
       <Header />
+      {/* Skapa menukomponenthär */}
       <Routes>
         <Route path="about" element={<AboutUs />} />
         <Route path="subscribe" element={<Subscribe />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </AppContainer>
   );
 }
 
