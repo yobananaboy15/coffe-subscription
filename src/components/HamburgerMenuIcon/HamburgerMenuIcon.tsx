8e8767bc4e27c3304ece3dbcfd62c6143334fca3
Axel Thiel <axth2220@gmail.com>
2021-11-17 19:57:43 +0100

    add menuprovider to hamburger component

--- src/components/HamburgerMenuIcon/HamburgerMenuIcon.tsx
+++ src/components/HamburgerMenuIcon/HamburgerMenuIcon.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './HamburgerMenuIcon.module.scss';
+import hamburgerIcon from '../../assets/shared/mobile/icon-hamburger.svg';
+import closeIcon from '../../assets/shared/mobile/icon-close.svg';
+import { useMenuProvider } from '../../context/MenuProvider';
 
-const HamburgerMenuIcon = () => (
-  <div className={styles.menuIcon}>
-    MENU
-  </div>
-);
+const HamburgerMenuIcon = () => {
+  const { isOpen } = useMenuProvider();
+  return (
+    <div className={styles.menuIcon}>
+      {isOpen ? <img src={closeIcon} alt="Close icon" /> : <img src={hamburgerIcon} alt="Hamburger icon" />}
+    </div>
+  );
+};
 
 export default HamburgerMenuIcon;
