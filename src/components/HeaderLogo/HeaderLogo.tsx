8d315cbe4075c8d3d88103727fa5686a1a0ebf13
Axel Thiel <axth2220@gmail.com>
2021-11-17 19:57:02 +0100

    add header logo

--- src/components/HeaderLogo/HeaderLogo.tsx
+++ src/components/HeaderLogo/HeaderLogo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import logo from '../../assets/shared/desktop/logo.svg';
+import styles from './HeaderLogo.module.scss';
 
 const HeaderLogo = () => (
-  <div>
-    Logo
-  </div>
+  <img src={logo} className={styles.logo} alt="Logo" />
 );
 
 export default HeaderLogo;
