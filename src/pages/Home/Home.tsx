ccfcdb99caaa57989894af8bf80cefa05f27b995
Axel Thiel <axth2220@gmail.com>
2021-11-22 14:25:13 +0100

    minor changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+import Button from '../../components/Button/Button';
 import ImageLg from '../../assets/home/desktop/image-hero-coffeepress.jpg';
 import ImageMd from '../../assets/home/tablet/image-hero-coffeepress.jpg';
 import ImageSm from '../../assets/home/mobile/image-hero-coffeepress.jpg';
@@ -11,16 +12,16 @@ const Home = () => {
   const { width } = useViewport();
   const heroImage = selectImage(width, { ImageSm, ImageMd, ImageLg });
   return (
-    <div>
-      <Hero heroImage={heroImage}>
-        <h2 className={styles.title}>Great coffee made simple</h2>
+    <Hero heroImage={heroImage}>
+      <div className={styles.heroContent}>
+        <h2 className={styles.title}>Great coffee made simple.</h2>
         <span className={styles.info}>
           Start your mornings with the world’s best coffees. Try our expertly curated artisan
           coffees from our best roasters delivered directly to your door, at your schedule.
         </span>
-        <button type="button">Test</button>
-      </Hero>
-    </div>
+        <Button text="Create your plan" callback={() => {}} />
+      </div>
+    </Hero>
   );
 };
 
